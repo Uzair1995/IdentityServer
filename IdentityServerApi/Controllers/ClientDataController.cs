6b2997d00e5e522d866619f6838ad1dfd1ee2762
Uzair Aleem <uzairaleem1995@gmail.com>
2021-08-16 15:53:09 +0500

    Added Client Data controller

---
+++ IdentityServerApi/Controllers/ClientDataController.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer.Repositories.Interfaces;
+using IdentityServer.Repositories.Models;
+using IdentityServer4.EntityFramework.DbContexts;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientDataController : ControllerBase
+    {
+        private readonly IClientDataRepository clientDataRepository;
+        private readonly ConfigurationDbContext configurationDbContext;
+
+        public ClientDataController(IClientDataRepository clientDataRepository, ConfigurationDbContext configurationDbContext)
+        {
+            this.clientDataRepository = clientDataRepository;
+            this.configurationDbContext = configurationDbContext;
+        }
+
+        [HttpPost("AddExtraClientData")]
+        public async Task<IActionResult> AddExtraClientData([FromBody] ClientData clientData)
+        {
+            var client = configurationDbContext.Clients.Where(x => x.ClientId.Equals(clientData.ClientId)).FirstOrDefault();
+
+            if (client == null)
+            {
+                return NotFound("Client does not exist.");
+            }
+
+            await clientDataRepository.InsertOrUpdateClientDataAsync(clientData);
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetClientDataByClientIdAsync(string clientId)
+        {
+            var clientData = await clientDataRepository.GetClientDataUsingId(clientId);
+            return Ok(clientData);
+        }
+    }
+}
